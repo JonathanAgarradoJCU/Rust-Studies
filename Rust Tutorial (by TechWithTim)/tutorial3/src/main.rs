692b58021f94244b918b11ba759eb80adb1a1b94
Jonathan Agarrado <jet320boy@gmail.com>
2024-01-23 14:13:04 +0800

    (Error) Learn keyword 'mutable'

--- Rust Tutorial (by TechWithTim)/tutorial3/src/main.rs
+++ Rust Tutorial (by TechWithTim)/tutorial3/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
     let x = 4;
     println!("The value of x is: {}", x);
+    x = 5;
+    println!("The value of x is: {}", x); // Error: cannot assign twice to immutable variable `x`
 }
